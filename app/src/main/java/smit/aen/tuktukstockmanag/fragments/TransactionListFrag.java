0e7974a3ad48a41163786d3a99ca4dff828cd5c0
senamit2708 <32201389+senamit2708@users.noreply.github.com>
2019-04-12 09:28:45 +0530

    material design changes two

--- app/src/main/java/smit/aen/tuktukstockmanag/fragments/TransactionListFrag.java
+++ app/src/main/java/smit/aen/tuktukstockmanag/fragments/TransactionListFrag.java
@@ -1,26 +1,36 @@
 package smit.aen.tuktukstockmanag.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import java.util.Calendar;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import smit.aen.tuktukstockmanag.Model.ProductM;
 import smit.aen.tuktukstockmanag.Model.TransactionModel;
 import smit.aen.tuktukstockmanag.R;
+import smit.aen.tuktukstockmanag.ViewModels.ProductViewM;
 import smit.aen.tuktukstockmanag.ViewModels.TransactionViewM;
 import smit.aen.tuktukstockmanag.adapter.TransactionListAdapter;
 
-public class TransactionListFrag extends Fragment {
+public class TransactionListFrag extends Fragment implements View.OnClickListener{
 
     private static final String TAG = TransactionListFrag.class.getSimpleName();
     private Context context;
@@ -30,12 +40,38 @@ public class TransactionListFrag extends Fragment {
     private RecyclerView.LayoutManager mLayoutManager;
 
     private TransactionViewM mViewModel;
+    private ProductViewM mProductViewModel;
+
+    private ImageButton btnDateFrom;
+    private ImageButton btnToDate;
+    private ImageButton btnProduct;
+
+    private static TextView txtFromDate;
+    private static TextView txtToDate;
+    private static TextView txtProduct;
+
+    private void bindView(View view) {
+
+        btnDateFrom = view.findViewById(R.id.btnDateFrom);
+        btnToDate = view.findViewById(R.id.btnToDate);
+        btnProduct = view.findViewById(R.id.btnProduct);
+        txtFromDate = view.findViewById(R.id.txtFromDate);
+        txtToDate = view.findViewById(R.id.txtToDate);
+        txtProduct = view.findViewById(R.id.txtProduct);
+
+        mRecyclerview = view.findViewById(R.id.recyclerview);
+        mAdapter = new TransactionListAdapter();
+        mLayoutManager = new LinearLayoutManager(context);
+        mRecyclerview.setLayoutManager(mLayoutManager);
+        mRecyclerview.setAdapter(mAdapter);
 
 
+    }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = ViewModelProviders.of(getActivity()).get(TransactionViewM.class);
+        mProductViewModel = ViewModelProviders.of(getActivity()).get(ProductViewM.class);
     }
 
     @Nullable
@@ -51,19 +87,93 @@ public class TransactionListFrag extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         bindView(view);
 
+        btnProduct.setOnClickListener(this);
+        btnToDate.setOnClickListener(this);
+        btnDateFrom.setOnClickListener(this);
+
         mViewModel.getCompleteTransList().observe(this, new Observer<List<TransactionModel>>() {
             @Override
             public void onChanged(List<TransactionModel> transList) {
                 mAdapter.setTransList(transList);
             }
         });
+
+        mProductViewModel.getSelectedProduct().observe(this, new Observer<ProductM>() {
+            @Override
+            public void onChanged(ProductM product) {
+                txtProduct.setText(product.getName());
+                //TODO: to do changes here
+            }
+        });
     }
 
-    private void bindView(View view) {
-        mRecyclerview = view.findViewById(R.id.recyclerview);
-        mAdapter = new TransactionListAdapter();
-        mLayoutManager = new LinearLayoutManager(context);
-        mRecyclerview.setLayoutManager(mLayoutManager);
-        mRecyclerview.setAdapter(mAdapter);
+
+    @Override
+    public void onClick(View v) {
+        if (v==btnDateFrom){
+            showDatePickerDialog();
+        }
+        if (v==btnToDate){
+            showDateToPickerDialog();
+        }
+        if (v==btnProduct){
+            Navigation.findNavController(v).navigate(R.id.action_transactionListFrag_to_productSearchListFrag);
+        }
+    }
+
+    private void showDateToPickerDialog() {
+        DialogFragment newFragment = new DatePickerFragTwo();
+        newFragment.show(getActivity().getSupportFragmentManager(), "datePickerTwo");
+    }
+
+    private void showDatePickerDialog() {
+        DialogFragment newFragment = new DatePickerFragOne();
+        newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragOne extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            //use the current date as the default date in the picker..
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            //create new instance of Datepicker dialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            String date = dayOfMonth+"-"+(month+1)+"-"+year;
+
+            txtFromDate.setText(date);
+        }
+    }
+
+
+    public static class DatePickerFragTwo extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            //use the current date as the default date in the picker..
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            //create new instance of Datepicker dialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            String date = dayOfMonth+"-"+(month+1)+"-"+year;
+
+            txtToDate.setText(date);
+        }
     }
 }
